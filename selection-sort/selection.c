dbb22a89090d574e0388729ba2e3e1a3c005e193
Davide Olianas <ubuntupk@gmail.com>
2018-05-19 17:57:11 +0200

    selection sort e funzioni di contorno

--- selection-sort/selection.c
+++ selection-sort/selection.c
@@ -1,32 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define ARRAY_SIZE 200
+#include <stdint.h>
+#include <inttypes.h>
+struct array_semplice {
+	int64_t * p_array; // p_array punta a interi con segno a 64bit
+	// ovvero da −(2^63) a 2^63 − 1
+	int64_t lunghezza; // (2^63 o.d.g 10^18 )
+	// se ogni numero occupa 64bit (== 8byte), una macchina con 8GB di RAM
+	// può contenere un miliardo di numeri (o.d.g 10^9)
+};
 
-void selection_sort(int (*)[]);
-
-// parray è un puntatore ad un array di interi
-// puntatore_a_funzione è puntatore a funzione che ritorna void e richiede come argomento un puntatore
-// ad array di int
-void esegui_ordinamento(void (*puntatore_a_funzione)( int (*)[] ), int (*parray)[] );
+void selection_sort(struct array_semplice *);
+void assegna_valori(struct array_semplice *);
+void esegui_ordinamento(void (*puntatore_a_funzione)(struct array_semplice *), struct array_semplice *);
+void stampa_array(struct array_semplice *);
 
 // https://stackoverflow.com/questions/859634/c-pointer-to-array-array-of-pointers-disambiguation
 
-int main(void ){
-	// dichiaro array come puntatore ad array di interi
-	int (* array)[];
-	// assegno valore ad array (salvo su array indirizzo di memoria iniziale dell'array)
-	array = (int (*)[]) malloc(sizeof(int) * ARRAY_SIZE);
-	// TODO assegnare valori all'array
-	esegui_ordinamento(selection_sort, array);	
+void assegna_valori(struct array_semplice * array){
+	for (int64_t i=0; i<(*array).lunghezza; i++){
+		int64_t * p_current = (*array).p_array + i;
+		*p_current = rand();
+	}
+}
+
+void stampa_array(struct array_semplice *arr){
+	for (int64_t i=0; i<(*arr).lunghezza; i++){
+		int64_t valore;
+		valore = *((*arr).p_array + i);
+		printf("%63li\n", valore);
+	}
+}
+
+void esegui_ordinamento(void (*puntatore_a_funzione)( struct array_semplice *), struct array_semplice * arr){
+	puntatore_a_funzione(arr);
+}
+
+// https://github.com/cofyc/argparse
+
+int main(int argc, char *argv[]){
+	srand(0);
+	int64_t array_size = 1;
+	if (argc == 2){
+		// al massimo ottengo 2^64-1
+		array_size = strtol(argv[1], NULL, 10);
+	};
+	struct array_semplice array;
+	array.lunghezza = array_size;
+	array.p_array = malloc(sizeof(int64_t) * array_size);
+
+	if (array.p_array == NULL){
+		printf("Non sono riuscito ad allocare l'array di dimensione %li\n", array_size);
+		return 1;
+	}
+
+	assegna_valori(&array);
+	esegui_ordinamento(selection_sort, &array);
+	//stampa_array(&array);
+	free(array.p_array);	
 	return 0;
 }
 
-void selection_sort(int (*x)[]){
-  int y;
-  y = (*x)[1];
-}
-
-void esegui_ordinamento(void (*puntatore_a_funzione)( int (*)[] ), int (*parray)[] ){
-	// TODO chiamare la funzione con l'array
-	puntatore_a_funzione(parray);
+void selection_sort(struct array_semplice *array){
+	for (int64_t i=0; i<(*array).lunghezza-1; i++){
+		int64_t * p_current;
+		p_current = (*array).p_array + i;
+		int64_t * p_lowest = p_current;
+		for (int64_t j=i+1; j<(*array).lunghezza; j++){
+			if ( *((*array).p_array + j) < *p_lowest ){
+				p_lowest = (*array).p_array + j;
+			}
+		}
+		if (p_lowest != p_current){
+			int64_t temp = *p_current;
+			*p_current = *p_lowest;
+			*p_lowest = temp;
+		}
+	}
 }
